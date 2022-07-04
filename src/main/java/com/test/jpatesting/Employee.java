d0585e33822d2f59147d327b6642792ed9ac7b91
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-04 08:05:16 +0530

    create project structure

--- src/main/java/com/test/jpatesting/Employee.java
+++ src/main/java/com/test/jpatesting/Employee.java
@@ -1,4 +1,24 @@
 package com.test.jpatesting;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long id;
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+    @Column(name = "email",nullable = false)
+    private String email;
 }
